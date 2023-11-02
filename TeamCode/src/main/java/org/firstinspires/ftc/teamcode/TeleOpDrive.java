5512f0956776f58911c6c69e6b11e0987c353b34
devrim6 <devrim6@users.noreply.github.com>
2023-11-02 18:10:06 +0200

    added todo list

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -22,6 +22,15 @@ public class TeleOpDrive extends LinearOpMode {
     mode currentMode = mode.TELEOP;
     @Override
     public void runOpMode() throws InterruptedException {
+        //TODO: the to do list
+        //masurare amperaj motoare glisiera pt determinare gear ratio optim la viteza, afaik max 4a, uitate pe spec sheet la rev
+        //recalibrare pozitie cu camera april tags la backboard, triunghiulare maybe? look into it, experimenteaza cate tag-uri se vad intrun frame
+        //daca localizarea ii accurate la sfarsit (cat de cat) incearca sa faci o traiectorie pt locul de lansat avion daca le trebuie la driveri, si pt hanging daca nu
+        //look into angular velocity tuning pt rotiri mai rapide si consistente
+        //dupa ce ii gata o autonomie, incearca sa bagi actiunile intrun alt fisier pt easier use
+        //led-uri pe cuva/text pe consola sa zica ce tip de pixel ii in care parte a robotului, gen culoare sau daca exista
+        //implementare senzori pt pixeli in cuva, daca sunt 2 in cuva driver 1/2 numai reverse poate da la intake motors/roller
+        //centrare pe april tag la backboard in teleop, depinde daca ne ajuta sau nu
         robot.init(hardwareMap);
         MecanumDrive drive = new MecanumDrive(hardwareMap, PoseTransfer.currentPose);
         // Init motors/servos/etc
