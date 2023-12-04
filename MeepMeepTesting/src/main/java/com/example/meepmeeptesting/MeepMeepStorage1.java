f678d978964961e03c118dfe8d0e43beedb2f254
Mara2oo7 <142086211+Mara2oo7@users.noreply.github.com>
2023-12-04 17:27:50 +0200

    Am facut o autonomie exact ca prima doar ca are cu un drum mai scurt

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepStorage1.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepStorage1.java
@@ -7,16 +7,18 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepStorage1 {
     MeepMeep meepMeep = new MeepMeep(600);
-    RoadRunnerBotEntity Autonomie1 = new DefaultBotBuilder(meepMeep)
+    RoadRunnerBotEntity Autonomie1Better = new DefaultBotBuilder(meepMeep)
             // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-            .setConstraints(35, 50, Math.toRadians(180), Math.toRadians(180), 15.1)
+            .setConstraints(45, 50, Math.toRadians(180), Math.toRadians(180), 15.1)
             .followTrajectorySequence(driveShim ->
                     driveShim.trajectorySequenceBuilder(new Pose2d(-34.5, -58 , Math.toRadians(90)))
 
                             .setReversed(false)
                             .splineToLinearHeading(new Pose2d(-34.5, -36, Math.toRadians(90)), Math.toRadians(90))
                             .setReversed(false)
+                            .waitSeconds(0.2)
                             .splineToLinearHeading(new Pose2d(-53,-36,Math.toRadians(0)),Math.toRadians(0))
+                            .waitSeconds(0.7)
                             .splineToLinearHeading(new Pose2d(-38.39, -12.02,Math.toRadians(0)), Math.toRadians(0))
                             .splineToLinearHeading(new Pose2d(36.39, -12.02,Math.toRadians(0)), Math.toRadians(0))
                             .splineToLinearHeading(new Pose2d(48.6, -35.63,Math.toRadians(0)), Math.toRadians(0))
@@ -42,4 +44,43 @@ public class MeepMeepStorage1 {
                             .splineToLinearHeading(new Pose2d(50, -12.02,Math.toRadians(0)), Math.toRadians(90))
                             .build()
             );
+
+    RoadRunnerBotEntity Autonomie2LessOK = new DefaultBotBuilder(meepMeep)
+            .setConstraints(45, 50, Math.toRadians(180), Math.toRadians(180), 15.1)
+            .followTrajectorySequence(driveShim ->
+                    driveShim.trajectorySequenceBuilder(new Pose2d(-34.5, -58 , Math.toRadians(90)))
+
+                            .setReversed(false)
+                            .splineToLinearHeading(new Pose2d(-34.5, -36, Math.toRadians(90)), Math.toRadians(90))
+                            .setReversed(false)
+                            .waitSeconds(0.2) //pune pixelul jos
+                            .splineToLinearHeading(new Pose2d(-53,-36,Math.toRadians(0)),Math.toRadians(0))
+                            .waitSeconds(0.7) //ia un pixel
+                            .splineToLinearHeading(new Pose2d(-38.39, -12.02,Math.toRadians(0)), Math.toRadians(0))
+                            .splineToLinearHeading(new Pose2d(36.39, -12.02,Math.toRadians(0)), Math.toRadians(0))
+                            .splineToLinearHeading(new Pose2d(48.6, -35.63,Math.toRadians(0)), Math.toRadians(0))
+                            .waitSeconds(0.7) //pune pixelii (prima oara)
+                            .setReversed(true)
+                            .splineToLinearHeading(new Pose2d(36.39, -12.02,Math.toRadians(0)), Math.toRadians(180))
+                            .splineToLinearHeading(new Pose2d(-49.2, -12.02,Math.toRadians(0)), Math.toRadians(180))
+                            .setReversed(false)
+                            .waitSeconds(0.7) //ia pixelii
+                            .splineToLinearHeading(new Pose2d(36.39, -12.02,Math.toRadians(0)), Math.toRadians(0))
+                            .splineToLinearHeading(new Pose2d(48.6, -35.63,Math.toRadians(0)), Math.toRadians(0))
+                            .waitSeconds(0.7) //pune pixelii (a doua oara)
+                            //al treilea drum
+                            .setReversed(true)
+                            /*.splineToLinearHeading(new Pose2d(36.39, -12.02,Math.toRadians(0)), Math.toRadians(180))
+                            .splineToLinearHeading(new Pose2d(-49.2, -12.02,Math.toRadians(0)), Math.toRadians(180))
+                            .setReversed(false)
+                            .waitSeconds(0.7)
+                            .splineToLinearHeading(new Pose2d(36.39, -12.02,Math.toRadians(0)), Math.toRadians(0))
+                            .splineToLinearHeading(new Pose2d(48.6, -35.63,Math.toRadians(0)), Math.toRadians(0))
+                            .waitSeconds(0.7)*/
+                            //de final
+                            .setReversed(true)
+                            .splineToLinearHeading(new Pose2d(50, -12.02,Math.toRadians(0)), Math.toRadians(90))
+                            .build()
+            );
+
 }
